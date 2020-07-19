92585210cedd59eb1c990435f397bc51ebd84515
wanxiaolu <88567982@qq.com>
2020-07-20 07:37:52 +0800

    1

---
+++ src/ch28/flexble_reflect_test.go
@@ -0,0 +1,20 @@
+package ch28
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDeepEqual(t *testing.T) {
+	a := map[int]string{1: "one", 2: "two", 3: "three"}
+	b := map[int]string{1: "one", 2: "two", 3: "three"}
+	//可以看出该比较会直接报错,表示无法进行比较
+	//t.Log(a == b)
+	t.Log(reflect.DeepEqual(a, b))
+
+	s1 := []int{1, 2, 3}
+	s2 := []int{1, 2, 3}
+	s3 := []int{2, 1, 3}
+	t.Log("s1 == s2?", reflect.DeepEqual(s1, s2))
+	t.Log("s2 == s3?", reflect.DeepEqual(s2, s3))
+}
